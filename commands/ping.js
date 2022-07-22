631b0907ebda32986a659f3d1ed8b57c4d114737
Артём Кэшов? <81440085+artemcashov@users.noreply.github.com>
2022-07-22 23:45:25 +0300

    Обновление кода

--- commands/ping.js
+++ commands/ping.js
@@ -1,19 +1,26 @@
-const { Client } = require('discord.js');
+const { Interaction, Client, MessageEmbed, MessageActionRow, MessageButton, MessageSelectMenu, Modal, TextInputComponent, Permissions } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
+const name = "пинг";
+const description = "Задержка бота олдж";
+
 module.exports = {
-  name: "ping", // имя команды
+  name: name, // имя команды
   types: ["CHAT", "SLASH"], // типы команды
-  description: "Задержка бота", // описание команды
+  description: description, // описание команды
   user_permissions: ["SEND_MESSAGES"], // требуемые права для пользователя
   user_roles: [], // требуемые роли для пользователя
   bot_permissions: ["SEND_MESSAGES"], // требуемые права для бота
-
+  componentsNames: [""],
   slash: new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription("Websocket пинг"),
+    .setName(name)
+    .setDescription(description),
 
   async execute(client, command) {
     command.reply(`🏓 Задержка ${Date.now() - command.createdTimestamp}ms. API: ${Math.round(client.ws.ping)}ms`);
+  },
+
+  componentListener(client, interaction) {
+    // do nothing
   }
 }
