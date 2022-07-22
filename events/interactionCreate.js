7043bab3f2e34564ac768efb8f7603a46821f07c
Артём Кэшов? <81440085+artemcashov@users.noreply.github.com>
2022-07-22 23:46:17 +0300

    Обновление кода

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -4,39 +4,49 @@ module.exports = {
   name: 'interactionCreate',
   async execute(client, interaction) {
     if (interaction.isCommand()) {
-      if (!interaction.guild) return;
       const cmd = client.commands.get(interaction.commandName)
       if (!cmd) return;
 
-      if (!cmd.types.includes("SLASH")) return
-
-      if (cmd.user_permissions) {
-        let invalidPerms = []
-        for (const perm of cmd.user_permissions) {
-          if (!interaction.member.permissions.has(perm)) invalidPerms.push(Perms[perm]);
+      if (cmd.types.includes("SLASH")) {
+        if (cmd.user_permissions) {
+          let invalidPerms = []
+          for (const perm of cmd.user_permissions) {
+            if (!interaction.member.permissions.has(perm)) invalidPerms.push(Perms[perm]);
+          }
+          if (invalidPerms.length) {
+            return await interaction.reply({ content: `У вас не достаточно прав: \`${invalidPerms}\``, ephemeral: true });
+          }
         }
-        if (invalidPerms.length) {
-          return await interaction.reply({ content: `У вас не достаточно прав: \`${invalidPerms}\``, ephemeral: true });
+  
+        if (cmd.user_roles && !cmd.user_roles[0] == "") {
+          if (!interaction.member.roles.cache.some(role => cmd.user_roles.includes(role.id))) {
+            return await interaction.reply({ content: "У вас нет требуемой роли", ephemeral: true });
+          }
         }
-      }
-
-      if (cmd.user_roles && !cmd.user_roles[0] == "") {
-        if (!interaction.member.roles.cache.some(role => cmd.user_roles.includes(role.id))) {
-          return await interaction.reply({ content: "У вас нет требуемой роли", ephemeral: true });
-        }
-      }
-
-      if (cmd.bot_permissions) {
-        let invalidPerms = []
-        for (const perm of cmd.bot_permissions) {
-          if (!interaction.guild.me.permissions.has(perm)) invalidPerms.push(Perms[perm]);
-        }
-        if (invalidPerms.length) {
-          return await interaction.reply({ content: `У меня не достаточно прав: \`${invalidPerms}\``, ephemeral: true });
+  
+        if (cmd.bot_permissions) {
+          let invalidPerms = []
+          for (const perm of cmd.bot_permissions) {
+            if (!interaction.guild.me.permissions.has(perm)) invalidPerms.push(Perms[perm]);
+          }
+          if (invalidPerms.length) {
+            return await interaction.reply({ content: `У меня не достаточно прав: \`${invalidPerms}\``, ephemeral: true });
+          }
         }
       }
 
       cmd.execute(client, interaction)
+    } else {
+      let found = false;
+      client.commands.forEach((cmd) => {
+          if(cmd.componentsNames) {
+              cmd.componentsNames.forEach(c => {
+                  if(interaction.customId.startsWith(c)) {
+                      cmd.componentListener(client, interaction)
+                  }
+              });
+          }
+      });
     }
   }
 }
